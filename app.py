0021e70d8211092d1008ecf089f6255ed0b7004c
Vi-Sri <nikola.tesla195@gmail.com>
2022-12-12 00:46:01 +0530

    integration edits

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from scribe import *
 from multiprocessing import Queue
+import openai
+import shelve
 
 app = Flask(__name__)
-
-CREDS = None
-CHAT = None
-FOLDERID = None
-TRANSCRIPT_ID = None
-REQUEST_DATETIME = None
+STORE = shelve.open("datastore")
 
 PROMPT_HEADER = f"""
         Summarize the following transcript. Write using the following format. Replace everything in <> brackets.
 
         Main Points:
-        - <main point 1> : <speaker 1>
-        - <main point 2> : <speaker 2>
-        - <and so on> : <and so on>
+        - <main point 1> : by <speaker 1>
+        - <main point 2> : by <speaker 2>
+        - <and so on> : by <and so on>
 
         Action Items:
-        - <action 1> : <speaker 1>
-        - <action 2> : <speaker 2>
-        - <and so on> : <and so on>
+        - <action 1> : by <speaker 1>
+        - <action 2> : by <speaker 2>
+        - <and so on> : by <and so on>
 
         Highights: 
         - <highlight 1>
@@ -49,27 +46,37 @@ def post_user_prompt():
     request_data = request.get_json()
     print(request_data)
     try:
-        REQUEST_DATETIME=request_data["date"]
-        TRANSCRIPT_ID = get_most_recent_transcript_id(CREDS, folder_id=FOLDERID, datetime=request_data["meeting"])
+        request_time=request_data["date"]
+        transcript_id = get_most_recent_transcript_id(STORE["google_creds"], folder_id=STORE["folder_id"], datetime=request_data["meeting"])
         print("##############")
-        print(TRANSCRIPT_ID)
+        print(transcript_id)
+        STORE["transcript_id"] = transcript_id
         print("##############")
     except HttpError as err:
         print(err)
     return jsonify(dict(response="200"))
 
 @app.route("/summary", methods=["GET"])
-def get_user_prompt():
-    doc_service = build('docs', 'v1', credentials=CREDS)
+def summary():
+    doc_service = build('docs', 'v1', credentials=STORE["google_creds"])
     action_items = []
     main_points = []
     highlights = []
-    document = doc_service.documents().get(documentId=TRANSCRIPT_ID).execute()
+    print("^^^^^^^^^^^^^^^^^^^^^^^^")
+    transcript_id = STORE["transcript_id"]
+    print(STORE["transcript_id"])
+    document = doc_service.documents().get(documentId=transcript_id['id']).execute()
     doc_content = document.get('body').get('content')
     transcript = read_structural_elements(doc_content)
     prompt = PROMPT_HEADER + transcript
-    answer = CHAT.ask(prompt)
-    resp_groups = split(answer[0], ['Main Points:', 'Action Items:', 'Highights:', 'Recent Summary:'])
+    # GENERATOR.generate(prompt=prompt,
+    #                engine='davinci',
+    #                max_tokens=20,
+    #                temperature=0.5,
+    #                top_p=1)
+    completion = openai.Completion.create(engine="davinci", prompt=prompt)
+    print(completion.choices[0].text)
+    resp_groups = split(completion.choices[0].text, ['Main Points:', 'Action Items:', 'Highights:', 'Recent Summary:'])
     
     for point in resp_groups[1].split('\n'):
         if point not in main_points:
@@ -92,7 +99,12 @@ def get_user_prompt():
 
 
 if __name__ == "__main__":
-    CREDS = get_credentials(force=False)
-    CHAT = get_gpt_chat(email="srinivas.thestallion.vishal@gmail.com", password="$Birth$1995$")
-    FOLDERID = get_folder(CREDS)
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    creds = get_credentials(force=False)
+    STORE["google_creds"] = creds
+    # CHAT = get_gpt_chat(email="srinivas.thestallion.vishal@gmail.com", password="$Birth$1995$")
+    openai.api_key = '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'
+    # set_api_key(KEY)
+    # GENERATOR = GPT3Generator(input_text="Template for Meeting summarization with meeting transcript", output_text="Filled Template")
+    folderid = get_folder(creds)
+    STORE["folder_id"] = folderid
+    app.run(host="0.0.0.0", port=8080)
